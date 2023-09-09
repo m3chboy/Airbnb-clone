cab48e997296eae735eee70856d4ee84ee182d62
Muhe <mukes.sethu@gmail.com>
2023-09-09 08:19:40 +0530

    img updated

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -44,7 +44,7 @@ export const Filter = () => {
                                     className='w-[24px] min-h-[23p] mb-1'
                                     src={filter.icon} alt="icon"
                                 />
-                                <p className='leading-normal text-[12px] font-semibold text-[#717171] whitespace-nowrap filter-under-active relative'>{filter.name}</p>
+                                <p className='leading-normal text-[12px] font-medium text-[#717171] whitespace-nowrap filter-under-active relative'>{filter.name}</p>
 
                             </div>
                         </Link>
